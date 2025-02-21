53280a2f8365d7189a44f5163add8b8c71b7a3b4
Shashank Raj <rajshashank2104@gmail.com>
2025-02-21 09:23:54 +0530

    added frontend part

---
+++ LaNeenos-Portal/src/core/common/commonSelect.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import Select from "react-select";
+
+export type Option = {
+  value: string;
+  label: string;
+};
+
+export interface SelectProps {
+  options: Option[];
+  defaultValue?: Option;
+  className?: string;
+  styles?: any; 
+  onChange?: (option: Option | null) => void;
+  name?: string;
+}
+
+const CommonSelect: React.FC<SelectProps> = ({ options, defaultValue, className,onChange,name}) => {
+  const [selectedOption, setSelectedOption] = useState<Option | undefined>(defaultValue);
+
+  const handleChange = (option: Option | null) => {
+    setSelectedOption(option || undefined);
+    if(onChange) {
+      onChange(option);
+    }
+  };
+  useEffect(() => {
+    setSelectedOption(defaultValue || undefined);
+  }, [defaultValue])
+  
+  return (
+    <Select
+     classNamePrefix="react-select"
+      className={className}
+      // styles={customStyles}
+      options={options}
+      value={selectedOption}
+      onChange={handleChange}
+      placeholder="Select"
+      name={name}
+    />
+  );
+};
+
+export default CommonSelect;
